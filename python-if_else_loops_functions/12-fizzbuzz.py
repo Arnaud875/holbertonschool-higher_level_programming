f3bdd1f5969b5ea865a261989a59e089dc528acb
Arnaud875 <arnaud.nickles33@gmail.com>
2024-09-05 13:41:13 +0200

    Update

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for i in range(1, 100, 1):
+    for i in range(1, 101, 1):
         result = ""
         if i % 3 == 0:
             result += "Fizz"
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
-def uppercase(str):
-    for i in str:
-        if ord(i) >= ord('a') and ord(i) <= ord('z'):
-            print(chr(ord(i) - 32), end="")
-        else:
-            print(i, end="")
-    print("\n")
+def uppercase(s):
+    result = ''.join([chr(ord(c) - 32) if 'a' <= c <= 'z' else c for c in s])
+    print(result)

a3fb588d2fb0b257df0baf2b4d3e269a931c2a82
Arnaud875 <arnaud.nickles33@gmail.com>
2024-09-06 11:15:24 +0200

    Fix task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     args = len(argv) - 1
     print("{:d} argument{:s}".format(args, "s:"
                                      if args > 1 else ":"
-                                     if args > 0 else "."))
+                                     if args > 0 else "s."))
 
     for i in range(args):
         print("{:d}: {}".format(i + 1, argv[i + 1]))

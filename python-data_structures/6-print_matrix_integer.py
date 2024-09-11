ea7eb6741dbfc2c505b03fb91ab1ac7e033f2c9c
Arnaud875 <arnaud.nickles33@gmail.com>
2024-09-11 17:50:31 +0200

    Fix all tasks for project 0

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if matrix == [[]]:
-        print()
+        print("")
     else:
         for i in matrix:
             length = len(i)
             for idx, j in enumerate(i):
                 if idx + 1 != length:
-                    print("{}".format(j), end=" ")
+                    print("{:d}".format(j), end=" ")
                 else:
-                    print("{}".format(j))
+                    print("{:d}".format(j))
--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    length = len(tuple_b)
+    length = len(tuple_b) - 1
     new_tuple = []
 
-    for i in range(len(tuple_a)):
+    for i in range(len(tuple_a) - 1):
         res = tuple_a[i]
         if i + 1 <= length:
             res += + tuple_b[i]

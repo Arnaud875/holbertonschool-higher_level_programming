77c84768481e9877a77135409362ea02e6c59d36
Arnaud875 <arnaud.nickles33@gmail.com>
2024-09-11 18:08:24 +0200

    Update task 3 | Project 2

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -6,5 +6,15 @@ def safe_print_division(a, b):
     except Exception as e:
         pass
     finally:
-        print("{:d}".format(result))
+        print("{}".format(result))
     return result
+
+a = 12
+b = 2
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))
+
+a = 12
+b = 0
+result = safe_print_division(a, b)
+print("{:d} / {:d} = {}".format(a, b, result))

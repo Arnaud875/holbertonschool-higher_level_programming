aa6e3540628bad297ac95fa306fefd7a6f043133
Arnaud875 <arnaud.nickles33@gmail.com>
2024-09-13 16:11:28 +0200

    Add unittest

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -1,5 +1,33 @@
 #!/usr/bin/python3
-"""Unittest max_integer(...)"""
-
+"""Unittest for max_integer([..])
+"""
 import unittest
 max_integer = __import__('6-max_integer').max_integer
+
+
+class TestMaxInteger(unittest.TestCase):
+    """Unit test for max_integer function"""
+
+    def test_positive_integers(self):
+        """Test with a list of positive integers"""
+        self.assertEqual(max_integer([1, 2, 3, 4]), 4)
+
+    def test_negative_integers(self):
+        """Test with a list of negative integers"""
+        self.assertEqual(max_integer([-1, -2, -3, -4]), -1)
+
+    def test_mixed_integers(self):
+        """Test with a list of mixed positive and negative integers"""
+        self.assertEqual(max_integer([1, -2, 3, -4]), 3)
+
+    def test_single_element(self):
+        """Test with a list of a single element"""
+        self.assertEqual(max_integer([7]), 7)
+
+    def test_empty_list(self):
+        """Test with an empty list"""
+        self.assertIsNone(max_integer([]))
+
+    def test_identical_elements(self):
+        """Test with a list of identical elements"""
+        self.assertEqual(max_integer([5, 5, 5]), 5)

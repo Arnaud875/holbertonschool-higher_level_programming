2ddb244de0820ec94a9c5ba3cdb8f69b3812257c
Arnaud875 <arnaud.nickles33@gmail.com>
2024-09-25 17:02:41 +0200

    Modifiy attributs for task 03

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -9,12 +9,22 @@ class CountedIterator:
     CountedIterator class
     """
     def __init__(self, data):
-        self.__data = iter(data)
+        self.iterator = iter(data)
         self.__count = 0
 
     def __next__(self):
         self.__count += 1
-        return next(self.__data)
+        return next(self.iterator)
 
     def get_count(self):
         return self.__count
+
+data = [1, 2, 3, 4]
+counted_iter = CountedIterator(data)
+
+try:
+    while True:
+        item = next(counted_iter)
+        print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
+except StopIteration:
+    print("No more items.")

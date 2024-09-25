6dfed6d674521dd75cb6e0ee92f1e22d70d4eb3a
Arnaud875 <arnaud.nickles33@gmail.com>
2024-09-25 17:06:59 +0200

    Fix task 03

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -18,13 +18,3 @@ class CountedIterator:
 
     def get_count(self):
         return self.__count
-
-data = [1, 2, 3, 4]
-counted_iter = CountedIterator(data)
-
-try:
-    while True:
-        item = next(counted_iter)
-        print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
-except StopIteration:
-    print("No more items.")
